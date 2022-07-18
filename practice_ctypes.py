3f5578b5d45350c3e15d481fbaecaba799bc9d7d
furafura1 <kakukakudaiou1@gmail.com>
2022-07-18 22:18:58 +0900

    test2

--- practice_ctypes.py
+++ practice_ctypes.py
@@ -1,9 +1,9 @@
 from ctypes import * 
 import numpy as np
-a = np.arange(0,1000,1)
+a = np.arange(0,100,1)
 liveadd = CDLL("./liveadd.so")
-size = c_int(1000)
-array = (c_double*1000) (*a.tolist())
+size = c_int(100)
+array = (c_double*100) (*a.tolist())
 result = (c_double*1) ()
 liveadd.add(size,array,result)
 print(result[0])
